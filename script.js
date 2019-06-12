2b7623f5686837d2db6765b8db4a89a3356af6a2
Jeffrey Hoglen <jeffhoglen@gmail.com>
2019-06-11 21:47:05 -0400

    links not working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("a").click(function(e) {
+$("a1,a2,a3").click(function(e) {
     e.preventDefault();
     var target = $(this).attr('href');   
     $('html, body').animate({ scrollTop : $(target).offset().top + "px"});
