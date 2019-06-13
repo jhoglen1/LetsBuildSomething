80d1e0c0e29ab1b8ab2de14dc9b702cb05392e46
Jeffrey Hoglen <jeffhoglen@gmail.com>
2019-06-13 18:41:13 -0400

    responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("a1,a2,a3").click(function(e) {
+$("About,Project,Contact").click(function(e) {
     e.preventDefault();
     var target = $(this).attr('href');   
     $('html, body').animate({ scrollTop : $(target).offset().top + "px"});
