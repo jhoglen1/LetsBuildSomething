9429cf5a98968b9dc85789228449476ab9ae93ac
Jeffrey Hoglen <jeffhoglen@gmail.com>
2019-06-13 23:37:34 -0400

    scroll

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ $( ".about" ).click(function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $('#About').offset().top
-    }, 1000);
+    }, 900);
 }); 
 
 $( ".projects" ).click(function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $('#Projects').offset().top
-    }, 1000);
+    }, 1250);
 }); 
 
 $( ".contact" ).click(function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $('#Contact').offset().top
-    }, 1000);
+    }, 4000);
 }); 
